2177206a4939cb1cb788af2274da4f2e00eb5119
catalin <cdojamihai@gmail.com>
2024-01-16 15:47:11 +0100

    Ultrasound part done with platformio
    Changes to make the sensors work properly.
    We add a verification byte to check the order of the data sent.

--- GSR_HR_BLE33/GSR_HR_BLE33.ino
+++ GSR_HR_BLE33/GSR_HR_BLE33.ino
@@ -1,27 +1,23 @@
 #include <Wire.h>
 #include "MAX30105.h"
 #include "heartRate.h"
-#include <Arduino.h>
 
 MAX30105 particleSensor;
+unsigned char toSend[4];
 
-
-
-const byte RATE_SIZE = 4; //Increase this for more averaging. 4 is good.
-byte rates[RATE_SIZE]; //Array of heart rates
+const byte RATE_SIZE = 4; // Increase this for more averaging. 4 is good.
+byte rates[RATE_SIZE];    // Array of heart rates
 byte rateSpot = 0;
-long lastBeat = 0; //Time at which the last beat occurred
+long lastBeat = 0;        // Time at which the last beat occurred
 
 float beatsPerMinute;
 int beatAvg;
-
-int state;
+long currentMoment = 0;
 
 void setup() {
-
-   Wire.begin(); //Join I2C bus
-  
+  Wire.begin(); // Join I2C bus
   Serial.begin(115200);
+  Serial1.begin(115200);
   Serial.println("Initializing...");
 
   // Initialize sensor
@@ -31,54 +27,57 @@ void setup() {
   }
   Serial.println("Place your index finger on the sensor with steady pressure.");
 
-  particleSensor.setup(); //Configure sensor with default settings
-  particleSensor.setPulseAmplitudeRed(0x0A); //Turn Red LED to low to indicate sensor is running
-  particleSensor.setPulseAmplitudeGreen(0); //Turn off Green LED
+  particleSensor.setup();                   // Configure sensor with default settings
+  particleSensor.setPulseAmplitudeRed(0x0A); // Turn Red LED to low to indicate sensor is running
+  particleSensor.setPulseAmplitudeGreen(0);  // Turn off Green LED
 }
 
 void loop() {
-  state = analogRead(A0);
-  Serial.print("Galvanic Skin Resistance=");
-  Serial.print(state);
-  Serial.println();
-  Wire.beginTransmission(0x2); // transmit to device address 8
-  Wire.write((byte *)&state, sizeof(state));
-  Wire.endTransmission();
+  toSend[3] = 111;
   long irValue = particleSensor.getIR();
+  int state = analogRead(A0);
+  
+  toSend[1] = (unsigned char)(state / 256);
+  toSend[2] = (unsigned char)(state % 256);
 
   if (checkForBeat(irValue) == true) {
-    //We sensed a beat!
+    // We sensed a beat!
     long delta = millis() - lastBeat;
     lastBeat = millis();
 
     beatsPerMinute = 60 / (delta / 1000.0);
 
     if (beatsPerMinute < 255 && beatsPerMinute > 20) {
-      rates[rateSpot++] = (byte)beatsPerMinute; //Store this reading in the array
-      rateSpot %= RATE_SIZE; //Wrap variable
+      rates[rateSpot++] = (byte)beatsPerMinute; // Store this reading in the array
+      rateSpot %= RATE_SIZE;                    // Wrap variable
 
-      //Take average of readings
+      // Take average of readings
       beatAvg = 0;
-      for (byte x = 0 ; x < RATE_SIZE ; x++)
+      for (byte x = 0; x < RATE_SIZE; x++)
         beatAvg += rates[x];
       beatAvg /= RATE_SIZE;
     }
   }
 
-  Serial.print("IR=");
-  Serial.print(irValue);
-  Serial.print(", BPM=");
-  Serial.print(beatsPerMinute);
-  Serial.print(", Avg BPM=");
-  Serial.print(beatAvg);
 
-  if (irValue < 50000){
-    Serial.print(" No finger?");
-  }else{
-  Wire.beginTransmission(2); // transmit to device address 8
-  Wire.write((byte *)&beatAvg, sizeof(beatAvg));
-  Wire.endTransmission();
-    }
-
+  if((millis()-currentMoment) > 500){
+  Serial.print("Galvanic Skin Resistance=");
+  Serial.print(state);
   Serial.println();
+  Serial.print(", BPM=");
+  toSend[0] = (unsigned char)beatsPerMinute;
+  Serial.println(beatsPerMinute);
+  Serial.println("Sent: " + String(toSend[0]) + " " + String(toSend[1]) + " " + String(toSend[2])+ " " + String(toSend[3]));
+  Serial1.write(toSend, 4);
+  Serial1.flush();
+  currentMoment = millis();
+  Serial.println();
+ 
+  }
+   
+ 
+
+
+
+  
 }
